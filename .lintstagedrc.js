4393a13209aaa8b04bd0c1249d8cf30b13a80066
Matt Kadlubowski <xeho91@pm.me>
2022-02-15 16:50:06 +0800

    chore(monorepo): 🔧  Improve workflow & update dependencies

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,13 @@
 // https://github.com/okonet/lint-staged
 
+function groupByPackagePath(filenames) {
+	const pattern = new RegExp("(?<=/)packages/.*?(?=/)");
+
+	return [
+		...new Set(filenames.map((filename) => filename.match(pattern)[0])),
+	];
+}
+
 const config = {
 	// Prettier
 	"*": ["pretty-quick --check --staged"],
@@ -7,16 +15,22 @@ const config = {
 	// ESLint
 	"*.{js,json,ts,yml,yaml}": ["eslint"],
 
-	// syncpack
-	"**/package.json": ["syncpack list-mismatches", "syncpack format"],
-
 	// markdownlint
 	"*.md": [`markdownlint --ignore "./.changeset/*.md"`],
 
-	// TypeScript types (in each package, because of different configs)
+	// Stylelint
+	"*.css": ["stylelint"],
+
+	// syncpack
+	"**/package.json": ["syncpack list-mismatches", "syncpack format"],
+
+	// TypeScript types (in each module/package, because of different configs)
 	"./helpers/**/*.ts": () => `tsc --project ./helpers/tsconfig.json --noEmit`,
-	// "./packages/logo/source/**/*.ts": () =>
-	// 	`tsc --project ./packages/logo/tsconfig.json --noEmit`,
+	"./packages/*/source/**/*.ts": (filenames) =>
+		groupByPackagePath(filenames).map(
+			(packagePath) =>
+				`tsc --project ./${packagePath}/tsconfig.json --noEmit`,
+		),
 };
 
 export default config;
